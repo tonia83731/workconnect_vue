83d99d82eb0dd8d881d8b3b73c2748766dcd6bb9
tonia83731 <huiyu.tonia.chang@gmail.com>
2025-11-25 15:53:41 +0800

    feat: add icons, add color pallette for light/dark mode, add dashboard header

---
+++ src/components/ButtonDefault.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+  const props = defineProps<{
+    // Add any props if needed in the future
+    style?: string;
+    onClick?: () => void;
+  }>()
+</script>
+<template>
+  <button :class="props.style" @click="props.onClick">
+    <slot></slot>
+  </button>
+</template>
+  <RouterView />
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,16 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { RouterLink } from 'vue-router'
+import MoonIcon from '@/components/icons/IconMoon.vue';
+import SunSolidIcon from '@/components/icons/IconSunSolid.vue';
+import LogoutIcon from '@/components/icons/IconLogout.vue';
+
+const theme = ref<'light' | 'dark'>('light');
+
+watch(theme, (newTheme) => {
+  document.documentElement.classList.toggle('dark', newTheme === 'dark');
+})
+
+console.log(theme.value)
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <header class="fixed top-0 left-0 w-full h-[45px] md:h-[60px] px-4 md:px-6 header-card flex items-center justify-between gap-4">
+    <router-link to="/" class="">
+      <h3>WORKCONNECT</h3>
+    </router-link>
+
+    <nav class="">
+      <!-- Dark/Light Mode -->
+      <button class="">
+        <component :is="theme === 'dark' ? MoonIcon : SunSolidIcon" />
+      </button>
+      <!-- Language -->
+      <!-- Logout -->
+       <button class="rotate-180">
+        <LogoutIcon :size="18"/>
+      </button>
+      <!-- User Info -->
+    </nav>
+
+  </header>
+
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
