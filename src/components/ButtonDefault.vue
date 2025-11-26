ba4ae98492ac3b90b0a35313ccc74fa4259ab33c
tonia83731 <huiyu.tonia.chang@gmail.com>
2025-11-26 21:06:04 +0800

    feat: completed ButtonDefault, add IconUser

--- src/components/ButtonDefault.vue
+++ src/components/ButtonDefault.vue
@@ -1,13 +1,80 @@
 <script setup lang="ts">
 
-  const props = defineProps<{
-    // Add any props if needed in the future
-    style?: string;
+  const props = withDefaults(
+  defineProps<{
+    type?: "button" | "submit";
+    shape?: keyof typeof shapeClasses;
+    color?: keyof typeof colorClasses;
+    class?: string;
     onClick?: () => void;
-  }>()
+  }>(),
+  {
+    type: "button",
+    shape: "default-md",
+    color: "primary",
+    class: "",
+  }
+)
+
+const rectSizeMap = {
+  sm: "px-2 py-1 text-sm",
+  md: "px-4 py-2 text-base",
+  lg: "px-6 py-3 text-lg",
+};
+
+const fixedSizeMap = {
+  sm: "w-6 h-6 md:w-8 md:h-8",
+  md: "w-8 h-8 md:w-10 md:h-10",
+  lg: "w-10 h-10 md:w-12 md:h-12",
+};
+
+const center = "flex items-center justify-center";
+
+const shapeClasses = {
+  // ----- Default -----
+  "default-sm": rectSizeMap.sm,
+  "default-md": rectSizeMap.md,
+  "default-lg": rectSizeMap.lg,
+
+  // ----- Circle -----
+  "circle-sm": `${fixedSizeMap.sm} rounded-full ${center}`,
+  "circle-md": `${fixedSizeMap.md} rounded-full ${center}`,
+  "circle-lg": `${fixedSizeMap.lg} rounded-full ${center}`,
+
+  // ----- Square -----
+  "square-sm": `${fixedSizeMap.sm} ${center}`,
+  "square-md": `${fixedSizeMap.md} ${center}`,
+  "square-lg": `${fixedSizeMap.lg} ${center}`,
+
+  // ----- Rounded -----
+  "rounded-sm": `${rectSizeMap.sm} rounded-sm`,
+  "rounded-md": `${rectSizeMap.md} rounded-md`,
+  "rounded-lg": `${rectSizeMap.lg} rounded-lg`,
+
+  // ----- Underline -----
+  "underline-sm": `${rectSizeMap.sm} underline underline-offset-2`,
+  "underline-md": `${rectSizeMap.md} underline underline-offset-2`,
+  "underline-lg": `${rectSizeMap.lg} underline underline-offset-2`,
+};
+
+
+const colorClasses = {
+  primary: "bg-action-background text-action-text disabled:bg-action-disabled",
+  secondary: "bg-action-background-secondary text-action-text-secondary disabled:bg-action-disabled-secondary",
+  "border-default": "border border-foreground text-foreground disabled:border-foreground-disabled disabled:text-foreground-disabled",
+  "border-primary": "border border-action-text text-action-text disabled:border-action-disabled disabled:text-action-disabled",
+  "text-default": "text-foreground disabled:text-foreground-disabled",
+  "text-primary": "text-action-text disabled:text-action-disabled",
+}
+
 </script>
 <template>
-  <button :class="props.style" @click="props.onClick">
+  <button
+    :type="props.type"
+    class="cursor-pointer"
+    :class="[shapeClasses[props.shape], colorClasses[props.color], props.class]"
+    @click="props.onClick"
+  >
     <slot></slot>
   </button>
 </template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 import { RouterLink } from 'vue-router'
-import MoonIcon from '@/components/icons/IconMoon.vue';
-import SunSolidIcon from '@/components/icons/IconSunSolid.vue';
-import LogoutIcon from '@/components/icons/IconLogout.vue';
+import IconMoon from '@/components/icons/IconMoon.vue';
+import IconSunSolid from '@/components/icons/IconSunSolid.vue';
+import IconLogout from '@/components/icons/IconLogout.vue';
+import IconUser from '@/components/icons/IconUser.vue';
+import ButtonDefault from '@/components/ButtonDefault.vue';
 
 const theme = ref<'light' | 'dark'>('light');
+const locale = ref<'en' | 'zhTW'>('en');
 
 watch(theme, (newTheme) => {
   document.documentElement.classList.toggle('dark', newTheme === 'dark');
 })
 
-console.log(theme.value)
 </script>
 <template>
   <header class="fixed top-0 left-0 w-full h-[45px] md:h-[60px] px-4 md:px-6 header-card flex items-center justify-between gap-4">
@@ -19,17 +21,24 @@ console.log(theme.value)
       <h3>WORKCONNECT</h3>
     </router-link>
 
-    <nav class="">
+    <nav class="flex items-center gap-2">
       <!-- Dark/Light Mode -->
-      <button class="">
-        <component :is="theme === 'dark' ? MoonIcon : SunSolidIcon" />
-      </button>
+       <button-default :shape="'circle-md'" :color="'border-default'">
+         <component :is="theme === 'dark' ? IconMoon : IconSunSolid" />
+       </button-default>
       <!-- Language -->
+        <button-default :shape="'circle-md'" :color="'border-default'">
+          <span v-if="locale === 'zhTW'">中</span>
+          <span v-else>EN</span>
+       </button-default>
       <!-- Logout -->
-       <button class="rotate-180">
-        <LogoutIcon :size="18"/>
-      </button>
+       <button-default :class="'rotate-180'" :shape="'circle-md'" :color="'border-default'">
+         <IconLogout :size="18"/>
+       </button-default>
       <!-- User Info -->
+       <button-default :class="'ml-2'" :shape="'circle-md'" :color="'border-default'">
+        <IconUser :size="18"/>
+       </button-default>
     </nav>
 
   </header>
